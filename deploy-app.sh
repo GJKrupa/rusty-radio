1a7e0348a89c55bdb5b4bbbf9cc3935226eec645
Gerard Krupa <gerard.krupa@capgemini.com>
2021-02-16 20:28:54 +0000

    Update to build on Ubuntu and updated a log of Angular versions

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 HOST=pi-radio.localdomain
+ARCH=armv7-unknown-linux-gnueabihf
 
-cargo build --target=armv7-unknown-linux-musleabihf
+cargo build --target=${ARCH}
 ssh "pi@${HOST}" "sudo systemctl stop rusty-radio"
-scp target/armv7-unknown-linux-musleabihf/debug/rusty-radio "pi@${HOST}:/home/pi/rusty-radio"
+scp target/${ARCH}/debug/rusty-radio "pi@${HOST}:/home/pi/rusty-radio"
 ssh "pi@${HOST}" "sudo systemctl start rusty-radio"
